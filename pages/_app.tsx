a88a9fab443aab4ff5c73ca1fc9adc1830b4bb59
rezagiovanni <rezagiovanni@users.noreply.github.com>
2026-07-30 07:48:03 +0700

    feat: add favicon using Avicenna logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo-avicenna.png" type="image/png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>E-Voting Avicenna</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
