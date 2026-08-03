e4188c3a21c1a5a733fb6be2368e83efa6ce1884
rezagiovanni <rezagiovanni@users.noreply.github.com>
2026-08-03 09:22:06 +0700

    remove: MPK B candidate option

--- pages/vote/mpk.tsx
+++ pages/vote/mpk.tsx
@@ -3,7 +3,6 @@ import CandidateCard from "@/components/CandidateCard";
 
 const MPK_CANDIDATES = [
   { id: "mpk_a", name: "Retno Ayu Anindita D. & Gandi Arka Riyadi", visi: "", img: "/candidates/mpk_a.jpg" },
-  { id: "mpk_b", name: "Calon MPK B", visi: "", img: "/candidates/mpk_b.jpg" },
 ];
 
 export default function VoteMPK() {
